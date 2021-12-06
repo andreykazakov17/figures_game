9248a18f33fd736c31fec134be507f989d2aa1ac
Andrew <andreyalexandrovich.k@gmail.com>
2021-12-06 20:28:15 +0200

    fixed canvas event listener & added score counter

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Определяем базовые классы
+// Определяем базовый класс квадратика
 
 class Rect {
 	constructor() {
@@ -8,6 +8,7 @@ class Rect {
 		this.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 		this.width = 20;
 		this.height = 20;
+		this.isActive = true;
 	}
 
 	draw(ctx) {
@@ -17,165 +18,91 @@ class Rect {
 	}
 }
 
-// class Canvas {
-// 	constructor(id) {
-// 		this.canvas = document.getElementById(id);
-		
-// 	}
 
-// 	add(...figures) {
-
-// 		let ctx = this.canvas.getContext("2d");
-// 		ctx.globalAlpha = 0.5;
-
-// 		for(let figure of figures) {
-// 			figure.draw(ctx);
-// 		}
-// 	}
-// }
-
+// Создание массива, инициализация переменных и получение элементов страницы
 
 let rectArr = [];
 
-// function updateArr() {
-// 	let rect = new Rect();
-// 	rectArr.push(rect);
-// 	//console.log(rectArr);
-// 	//return rectArr;
-// }
-
-//-- генерация рандомного числа фукцией
-
-// function randomTime(min, max) {
-// 	return (max-min)*Math.random() + min;
-// }
-
-//--
-
-
 let rand = Math.round(Math.random() * (6000 - 500)) + 500;
-//let timerID = setInterval(updateArr, rand);
 let timerID;
 
 let startBtn = document.querySelector(".start");
 let stopBtn = document.querySelector(".stop");
 let canvasElem = document.getElementById("canvas");
+let scoreSpan = document.getElementById("score");
+let scoreCounter = 0;
 
-// function startHandler() {
-// 	timerID = setTimeout(function() {
-// 		updateArr();
-// 		startHandler();
-// 	}, rand);
-// }
+
+// Обработчики событий
 
 function startHandler() {
 	
+	scoreCounter = 0;
+	scoreSpan.innerHTML = `${scoreCounter}`;
+
 	timerID = setInterval(function() {
 		let rect = new Rect();
 		rectArr.push(rect);
-		
-		//startHandler();
 	}, rand);
 }
 
 function stopHandler() {
 	clearInterval(timerID, 1000);
 	timerID = null;
+
+	for(let obj of rectArr) {
+		obj.isActive = false;
+	}
 }
 
 startBtn.addEventListener("click", startHandler);
 stopBtn.addEventListener("click", stopHandler);
 
+// Обработка кликов по квадратикам
 
-// function callDraw() {
-// 	rectArr.forEach(obj => {
-// 		obj.draw();
-// 	});
-// }
-// callDraw();
-
-
-//-- IIFE 
-
-// (function loop() {
-// 	let rand = Math.round(Math.random() * (6000 - 500)) + 500;
-// 	setTimeout(function() {
-// 		updateArr();
-// 		loop();  
-// 	}, rand);
-// }());
-
-//--
-
-
-// function draw() {
-// 	ctx.clearRect(0,0, canvas.width, canvas.height);
-// 	ball.draw();
-// 	ball.x += ball.vx;
-// 	ball.y += ball.vy;
-// 	raf = window.requestAnimationFrame(draw);
-// }
+function getMousePos(canvas, evt) {
+	let rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
 
 canvasElem.addEventListener("click", (event) => {
 
-	let eventX = event.offsetX;
-	let eventY = event.offsetY;
+	let pos = getMousePos(canvasElem, event);
 
 	for(let obj of rectArr) {
-		if((obj.x < eventX < obj.x + obj.width) && (obj.y < eventY < obj.y + obj.height)) {
-			for(let value of rectArr) {
-				rectArr.splice(rectArr.indexOf(value), 1);
-			}
+		if(pos.x >= obj.x && pos.x <= (obj.x + obj.width) && pos.y >= obj.y && pos.y <= (obj.y + obj.height)) {
+			obj.isActive = false;
+			scoreCounter += 1;
+			scoreSpan.innerHTML = `${scoreCounter}`;
 		}
 	}
 });
+
+
+// Прорисовка анимации
 
 function animate() {
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
 
 	ctx.clearRect(0,0, canvas.width, canvas.height);
-	// тут может находиться ваш код
+
+	let filterArr = rectArr.filter((obj) => obj.isActive ===true && obj.y < canvasElem.height);
 
 
-	let filterArr = rectArr.filter((obj) => {
-		if(obj.y > canvasElem.height) {
-			rectArr.splice(rectArr.indexOf(obj), 1);
-		}
-	});
-	console.log(filterArr);
+	for(let figure of filterArr) {
 
-
-
-
-	if(rectArr.length !== 0) {
-		for(let figure of rectArr) {
+		if(figure.isActive === true) {
 			figure.draw(ctx);
-			//figure.y = figure.y + figure.speed;
-			// canvasElem.addEventListener("click", (event) => {
-
-			// 	let eventX = event.offsetX;
-			// 	let eventY = event.offsetY;
-			
-			// 	if((figure.x < eventX < figure.x + figure.width) && (figure.y < eventY < figure.y + figure.height)) {
-			// 		rectArr.splice(rectArr.indexOf(figure), 1);
-			// 	}
-			// });
 		}
-		console.log(rectArr);
-	}
-
-	
-	// for(let figure of rectArr) {
-	// 	figure.draw(ctx);
-	// 	//console.log(figure);
 		
-	// }
-	// console.log(rectArr);
+	}
+	console.log(filterArr);
 
 	requestAnimationFrame(animate);
 }
-
-// тут может находиться ваш код
 
 document.body.onload = animate;
